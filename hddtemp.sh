f692eb1bc89107343be1e0e6d82d945099a9dfdc
Levente Csikor <lele@nidoqueen.comp.nus.edu.sg>
2020-10-29 20:49:31 +0800

    update

--- hddtemp.sh
+++ hddtemp.sh
@@ -6,4 +6,4 @@ HOST=nidoqueen
 #timestamp=$(date +%s)000000000
 
 temp=$(sudo hddtemp -n -w $DEV)
-echo "my_hddtemp,host=${HOST} temperature=${temp}"
+echo "my_hddtemp,host=${HOST} temperature=${temp},temp_${HOST}=${temp}"
